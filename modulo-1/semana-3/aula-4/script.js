7138c717c7b8f832b66affb86df181d9b6d23c4c
romeuic <casarottori@gmail.com>
2022-03-17 21:42:24 -0300

    adiciona exemplor timeout interval m1-s3-a4

--- modulo-1/semana-3/aula-4/script.js
+++ modulo-1/semana-3/aula-4/script.js
@@ -1,2 +1,24 @@
 
-console.log('Está funcionando!');
+function falaOi() {
+  console.count('Oi');
+}
+
+//var tOi = setTimeout(falaOi, 2000);
+
+//clearTimeout(tOi);
+
+// var iOi = setInterval(falaOi, 1000);
+
+// setTimeout(function () {
+//   console.log('Limpou o interval!');
+//   clearInterval(iOi);
+// }, 4000);
+
+// var i = 1;
+// var intervalOi = setInterval(function () {
+//   falaOi();
+//   i++;
+//   if (i > 3) {
+//     clearInterval(intervalOi);
+//   }
+// }, 1000);
