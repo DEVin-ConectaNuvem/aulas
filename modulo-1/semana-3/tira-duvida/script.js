f9e04925a986aad02c1cf94fe00a6e522276051a
romeuic <casarottori@gmail.com>
2022-03-18 19:54:30 -0300

    adiciona exemplo json m1-s3-a5

--- modulo-1/semana-3/tira-duvida/script.js
+++ modulo-1/semana-3/tira-duvida/script.js
@@ -1,9 +1,12 @@
 // aula de objetos/JSON/localStorage tira-duvidas
 
+var p = document.getElementById('display');
+
 var carro = {
   marca: 'ford',
   modelo: 'escort',
   ano: 1995,
+  dono: false,
   revisoes: [
     {
       data: '01/01/2022',
@@ -25,6 +28,26 @@ carro.revisoes[0].descricao = 'MUDEI';
 //  console.log(item);
 // });
 
-for (var key in carro) {
-  console.log(key);
-}
+// for in me dá keys/indexes
+// for (var key in carro) {
+//   console.log(carro[key]);
+// }
+
+// for of se usa apenas com array me dá os valores
+// for (var item of Object.entries(carro)) {
+//   console.log(item);
+// }
+
+console.log(carro);
+
+// transforma objeto em string JSON
+var carroJSON = JSON.stringify(carro);
+
+console.log(carroJSON);
+
+p.innerHTML = carroJSON;
+
+// transforma string JSON em objeto
+var carroObj = JSON.parse(carroJSON);
+
+console.log(carroObj);
