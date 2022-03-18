4aad2f8c8ebf05d3aa222e6d7dae1d46cfff5788
romeuic <casarottori@gmail.com>
2022-03-18 19:29:10 -0300

    adiciona exemplo de objeto m1-s3-a5

--- modulo-1/semana-3/tira-duvida/script.js
+++ modulo-1/semana-3/tira-duvida/script.js
@@ -1,1 +1,21 @@
 // aula de objetos/JSON/localStorage tira-duvidas
+
+var carro = {
+  marca: 'ford',
+  modelo: 'escort',
+  ano: 1995,
+  revisoes2: [
+    {
+      data: '01/01/2022',
+      descricao: 'trocado oleo'
+    },
+    {
+      data: '15/06/2021',
+      descricao: 'trocado pneus'
+    }
+  ]
+};
+
+console.log(carro);
+console.log(carro.modelo);
+console.log(carro.revisoes2[0].descricao);
