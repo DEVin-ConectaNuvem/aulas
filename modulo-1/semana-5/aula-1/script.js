cd697fdcde06ddaa57528dcf32b8e02f2c9c5ffa
romeuic <casarottori@gmail.com>
2022-03-28 20:29:30 -0300

    atualiza exemplo m1-s5-a1

--- modulo-1/semana-5/aula-1/script.js
+++ modulo-1/semana-5/aula-1/script.js
@@ -46,43 +46,47 @@
 // ARROW FUNCTIONS
 
 // declaração clássica
-// function saudacoes() {
-//   console.log('Olá! A');
-// }
-// const saudacoesComum = function () {
-//   console.log('Olá! Comum');
-// }
-// // declaração arrow
-// const saudacoesArrow = () => {
-//   console.log('Olá! =>');
-// }
+function saudacoes() {
+  console.log('Olá! A');
+}
+const saudacoesComum = function () {
+  console.log('Olá! Comum');
+}
+// declaração arrow
+const saudacoesArrow = () => {
+  console.log('Olá! =>');
+}
 
 // saudacoes();
 // saudacoesComum();
 // saudacoesArrow();
 
-// const vetor = [1, 2, 3, 4, 5];
+const vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// // vetor.forEach(function (item) {
-// //   console.log(item);
-// //   console.log('a');
-// // })
+// vetor.forEach(function (item) {
+//   console.log(item);
+//   console.log('a');
+// })
 
 // vetor.forEach(
 //   item => console.log(item)
 // )
 
-// function quadrado(numero) {
-//   return numero ** 2;
-// }
-// const quadrado = function (numero) {
-//   return numero ** 2;
-// }
-// const quadrado = numero => {
-//   return numero ** 2;
-// }
-const quadrado = numero => {
+// funções equivalentes
+function quadradoA(numero) {
   return numero ** 2;
-};
+}
+const quadradoB = function (numero) {
+  return numero ** 2;
+}
+const quadradoC = numero => {
+  return numero ** 2;
+}
 
-console.log(quadrado(2));
+// arrow function sem chaves retorna
+// automaticamente o valor da instrução
+const quadrado = numero => numero ** 2;
+const resultado = quadrado(2);
+//console.log(resultado);
+
+vetor.forEach(item => console.log(item))
