02a4e2ac2dfdf9b033ec204ba1124e8a619936b7
romeuic <casarottori@gmail.com>
2022-03-29 21:01:24 -0300

    atualiza exemplo m1-s5-a2

--- modulo-1/semana-5/aula-2/script.js
+++ modulo-1/semana-5/aula-2/script.js
@@ -1,6 +1,9 @@
 
-import { testeExport, Pessoa } from './utils.js'
+import Blah, { daTchau, daOi as ola } from './utils.js'
 
-const romeu = new Pessoa('Romeu', 29);
-
+//console.log(testeExport);
+const romeu = new Blah('Romeu', 29); // default
 console.log(romeu);
+
+ola(); // função renomeada
+daTchau(); // função com mesmo nome
--- modulo-1/semana-5/aula-2/utils.js
+++ modulo-1/semana-5/aula-2/utils.js
@@ -1,9 +1,25 @@
 
 export const testeExport = 42;
 
-export class Pessoa {
+export default class Pessoa {
   constructor(nome, idade) {
     this.nome = nome;
     this.idade = idade;
   }
 }
+
+export function daOi() {
+  console.log('Oi!');
+}
+
+export function daTchau() {
+  console.log('Tchau!');
+}
+
+//const utilitarios = {
+//   daOi,
+//   testeExport,
+//   Pessoa
+// }
+
+// export default utilitarios
