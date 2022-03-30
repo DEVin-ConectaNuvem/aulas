06a26c1a3c17093c2259b182d21002319be7b348
romeuic <casarottori@gmail.com>
2022-03-29 21:12:51 -0300

    atualiza exemplo m1-s5-a2

--- modulo-1/semana-5/aula-2/script.js
+++ modulo-1/semana-5/aula-2/script.js
@@ -1,9 +1,26 @@
 
-import Blah, { daTchau, daOi as ola } from './utils.js'
+import Pessoa, { daTchau, daOi as ola } from './utils.js'
 
 //console.log(testeExport);
-const romeu = new Blah('Romeu', 29); // default
-console.log(romeu);
+const romeu = new Pessoa('Romeu', 29); // default
+//console.log(romeu);
 
-ola(); // função renomeada
-daTchau(); // função com mesmo nome
+//ola(); // função renomeada
+//daTchau(); // função com mesmo nome
+
+
+
+// EXEMPLOS OPERADOR REST
+
+// function apresenta(nome, ...resto) {
+//   console.log(`Olá, ${nome}(${resto})`);
+// }
+// apresenta('Romeu', 29, 'Programador', 'Professor');
+
+function somaTudo(a, b, c, ...numeros) {
+  console.log(a, b, c);
+  return numeros.reduce((acc, num) => acc + num, 0)
+}
+
+const result = somaTudo(2, 45, 3, 33, 78);
+console.log(result);
