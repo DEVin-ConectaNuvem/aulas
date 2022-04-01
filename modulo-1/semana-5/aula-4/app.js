73cf4c6383d68fd5e9dc6c10d1160f9e816f080c
romeuic <casarottori@gmail.com>
2022-03-31 21:47:53 -0300

    altera exemplo m1-s5-a4 cat api

--- modulo-1/semana-5/aula-4/app.js
+++ modulo-1/semana-5/aula-4/app.js
@@ -157,3 +157,26 @@ async function buscaCEPtry() {
 }
 
 btnCEP.addEventListener('click', buscaCEPtry);
+
+
+// EXEMPLO THE CAT API
+
+const btnCat = document.getElementById('btn-cat');
+const imgCat = document.getElementById('img-cat');
+
+async function fetchCat() {
+  try {
+
+    const url = 'https://api.thecatapi.com/v1/images/search';
+    const resposta = await fetch(url);
+    const conteudo = await resposta.json()
+
+    console.log({ conteudo });
+    imgCat.src = conteudo[0].url;
+
+  } catch (erro) {
+    console.error(erro);
+  }
+}
+
+btnCat.addEventListener('click', fetchCat);
