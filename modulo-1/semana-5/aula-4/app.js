b451f5bfccf1ccde54c48dcdf9e431894e6723d1
romeuic <casarottori@gmail.com>
2022-03-31 21:17:00 -0300

    atualiza exemplo m1-s5-a4 async await

--- modulo-1/semana-5/aula-4/app.js
+++ modulo-1/semana-5/aula-4/app.js
@@ -56,7 +56,7 @@ const display = document.getElementById('display');
 const campoCEP = document.getElementById('campo-cep');
 const btnCEP = document.getElementById('btn-cep');
 
-function buscaCEP() {
+function buscaCEPfetch() {
   const cep = campoCEP.value;
 
   if (!cep) {
@@ -72,11 +72,56 @@ function buscaCEP() {
       })
     })
     .catch(erro => {
-      console.log(erro)
+      console.log(erro);
     });
 }
 
-btnCEP.addEventListener('click', buscaCEP);
+//btnCEP.addEventListener('click', buscaCEPfetch);
 
 
 // EXEMPLOS ASYNC AWAIT
+
+// promise com atraso
+function delay(ms) {
+  return new Promise(resolve => {
+      setTimeout(() => resolve('blah'), ms);
+    }
+  );
+}
+
+// uma arrow function assíncrona
+async function buscaCEP() {
+  // comando de espera
+  const resposta = await delay(1000);
+  // executa após conclusão de delay
+  console.log('Terminei de esperar! ' + resposta);
+}
+
+// roda o exemplo
+// buscaCEP();
+// imprime primeiro no console
+// console.log('Executei o console log');
+
+// async await com fetch
+
+async function buscaCEPawait() {
+  const cep = campoCEP.value;
+
+  if (!cep) {
+    display.innerHTML = 'Informe um CEP!';
+    return;
+  }
+
+  const url = `https://viacep.com.br/ws/${cep}/json`;
+  const resposta = await fetch(url);
+  const conteudo = await resposta.json()
+
+  console.log(conteudo);
+  display.innerHTML = conteudo.localidade;
+
+  // .catch(erro => {
+  //   console.log(erro);
+  // });
+}
+
+btnCEP.addEventListener('click', buscaCEPawait);
