6d4e545a4d406b639c6f57c6fb18c2fedeef0b53
romeuic <casarottori@gmail.com>
2022-04-01 21:44:58 -0300

    adiciona exemplo m1s05 ex-10

---
+++ modulo-1/semana-5/exercicios/ex-10/app.js
@@ -0,0 +1,25 @@
+const btnCat = document.getElementById('btn-cat');
+const imgCat = document.getElementById('img-cat');
+
+// https://api.thecatapi.com/v1/images/search
+const URL = 'https://api.thecatapi.com/v1/images/search';
+
+async function showMeTheCat() {
+  try {
+
+    const response = await fetch(URL);
+    //console.log(response);
+    const content = await response.json();
+    console.log(content);
+
+    imgCat.src = content[0].url;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+btnCat.addEventListener('click', showMeTheCat);
+
+// showMeTheCat();
+// ou
+window.onload = showMeTheCat;
